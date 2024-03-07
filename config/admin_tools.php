a43c6263cae482f19a1055c43906dde862d1b657
Alberto Rodriguez <arodu.dev@gmail.com>
2024-03-07 12:47:39 -0400

    Update backup email configuration in admin_tools.php

--- config/admin_tools.php
+++ config/admin_tools.php
@@ -3,18 +3,19 @@
 return [
     'AdminTools' => [
         'backup' => [
+            'name' => env('AT_BACKUP_NAME', 'default'),
             'datasource' => env('AT_BACKUP_DATASOURCE', 'default'),
             'compress' => env('AT_BACKUP_COMPRESS', 'gzip'),
             'path' => env('AT_BACKUP_PATH', TMP),
             'email' => [
-                'enable' => env('AT_BACKUP_EMAIL_ENABLE', true),
-                'config' => env('AT_BACKUP_EMAIL_CONFIG', 'default'), // email config to use
-                'format' => env('AT_BACKUP_EMAIL_FORMAT', 'both'), // html, text, both
-                'emailList' => env('AT_BACKUP_EMAIL_LIST', []),
-                'subject' => env('AT_BACKUP_EMAIL_SUBJECT', null),
-                'template' => env('AT_BACKUP_EMAIL_TEMPLATE', 'AdminTools.default'),
-                'layout' => env('AT_BACKUP_EMAIL_LAYOUT', 'default'),
-                'removeFileAfterSend' => env('AT_BACKUP_EMAIL_REMOVE_FILE', true),
+                'enable' => env('AT_BACKUP_EMAIL_ENABLE') ?? true,
+                'config' => env('AT_BACKUP_EMAIL_CONFIG') ?? 'default', // email config to use
+                'format' => env('AT_BACKUP_EMAIL_FORMAT') ?? 'both', // html, text, both
+                'emailList' => env('AT_BACKUP_EMAIL_LIST') ?? [],
+                'subject' => env('AT_BACKUP_EMAIL_SUBJECT') ?? null,
+                'template' => env('AT_BACKUP_EMAIL_TEMPLATE') ?? 'AdminTools.default',
+                'layout' => env('AT_BACKUP_EMAIL_LAYOUT') ?? 'default',
+                'removeFileAfterSend' => env('AT_BACKUP_EMAIL_REMOVE_FILE') ?? true,
             ],
         ],
     ],
