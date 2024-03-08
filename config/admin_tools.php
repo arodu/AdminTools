ca98f8722ea239830ae8490cf274e1001bdb2e53
Alberto Rodriguez <arodu.dev@gmail.com>
2024-03-08 10:41:51 -0400

    Refactor backup configuration in admin_tools.php

--- config/admin_tools.php
+++ config/admin_tools.php
@@ -6,55 +6,51 @@ return [
     'AdminTools' => [
         'backup' => [
             /**
+             * @var bool
+             * 
              * Enable or disable the backup
              */
             'enabled' => env('AT_BACKUP_ENABLED') ?? true,
 
             /**
+             * @var string
+             * 
              * The name of the backup
              */
             'name' => env('AT_BACKUP_NAME') ?? 'backup',
 
             /**
+             * @var string
+             * 
              * A list of datasources to backup, you can use an array or a string separated by comma
              */
             'datasources' => env('AT_BACKUP_DATASOURCES') ?? 'default',
 
             /**
-             * Extra options to use for each datasource
+             * @var string|null|false
              * 
-             * Example:
-             * 'datasourceOptions' => [
-             *     'default' => [
-             *         'name' => 'otherNameBackup',
-             *         'path' => 'other/path/to/save/backup',
-             *         'compress' => 'zip',
-             *         'removeFile' => false,
-             *         'email' => [
-             *             'enabled' => false,
-             *         ],
-             *     ],
-             * ],
-            */
-            'datasourceOptions' => [],
-
-            /**
              * The compress method to use
              * gzip, zip, rar, none
              */
             'compress' => env('AT_BACKUP_COMPRESS') ?? 'gzip',
 
             /**
+             * @var string
+             * 
              * The path where the backup file will be saved
              */
             'path' => env('AT_BACKUP_PATH') ?? TMP,
 
             /**
+             * @var bool
+             * 
              * Remove the file after execution of the command
              */
             'removeFile' => env('AT_BACKUP_REMOVE_FILE') ?? true,
 
             /**
+             * @var callable|null|false
+             * 
              * You can use a callable to execute a script before dump the file
              * This script runs by each datasource, or you can change it by datasource in datasourceOptions
              * function (array $options, ConsoleIo $io): ?string {
@@ -66,6 +62,8 @@ return [
             'dumpScript' => null,
 
             /**
+             * @var callable|null|false
+             * 
              * This anonymous function is a callback that is executed after a script is executed.
              * This script runs by each datasource, or you can change it by datasource in datasourceOptions
              * It takes an array of options and a ConsoleIo object as parameters.
@@ -81,43 +79,74 @@ return [
              */
             'afterExecScript' => null,
 
-            'email' => [
-                /**
-                 * Enable or disable the email backup
-                 */
-                'enabled' => env('AT_BACKUP_EMAIL_ENABLE') ?? true,
+            /**
+             * @var array|null
+             * 
+             * Extra options to use for each datasource
+             * this options will override the default options
+             * 
+             * Example:
+             * 'datasourceOptions' => [
+             *     'default' => [
+             *         'name' => 'otherNameBackup',
+             *         'path' => 'other/path/to/save/backup',
+             *         'compress' => 'zip',
+             *         'removeFile' => false,
+             *     ],
+             * ],
+             */
+            'datasourceOptions' => [],
+        ],
+        'backupEmail' => [
+            /**
+             * @var bool
+             * 
+             * Enable or disable the email backup
+             */
+            'enabled' => env('AT_BACKUP_EMAIL_ENABLE') ?? true,
 
-                /**
-                 * The email configuration to use
-                 */
-                'config' => env('AT_BACKUP_EMAIL_CONFIG') ?? 'default',
+            /**
+             * @var string
+             * 
+             * The email configuration to use
+             */
+            'config' => env('AT_BACKUP_EMAIL_CONFIG') ?? 'default',
 
-                /**
-                 * The email format to use
-                 * html, text, both
-                 */
-                'format' => env('AT_BACKUP_EMAIL_FORMAT') ?? 'both',
+            /**
+             * @var string
+             * 
+             * The email format to use
+             * html, text, both
+             */
+            'format' => env('AT_BACKUP_EMAIL_FORMAT') ?? 'both',
 
-                /**
-                 * The email recipients
-                 */
-                'emailList' => env('AT_BACKUP_EMAIL_LIST') ?? '',
+            /**
+             * @var array|string|null
+             * 
+             * The email recipients
+             */
+            'emailList' => env('AT_BACKUP_EMAIL_LIST') ?? null,
 
-                /**
-                 * The email subject
-                 */
-                'subject' => env('AT_BACKUP_EMAIL_SUBJECT') ?? null,
+            /**
+             * @var string|null
+             * 
+             * The email subject
+             */
+            'subject' => env('AT_BACKUP_EMAIL_SUBJECT') ?? null,
 
-                /**
-                 * The email template
-                 */
-                'template' => env('AT_BACKUP_EMAIL_TEMPLATE') ?? 'AdminTools.default',
+            /**
+             * @var string
+             * 
+             * The email template
+             */
+            'template' => env('AT_BACKUP_EMAIL_TEMPLATE') ?? 'AdminTools.default',
 
-                /**
-                 * The email layout
-                 */
-                'layout' => env('AT_BACKUP_EMAIL_LAYOUT') ?? 'default',
-            ],
+            /**
+             * @var string
+             * 
+             * The email layout
+             */
+            'layout' => env('AT_BACKUP_EMAIL_LAYOUT') ?? 'default',
         ],
     ],
 ];
